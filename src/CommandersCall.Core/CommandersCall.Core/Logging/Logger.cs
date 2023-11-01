c1dba3070359db30c40091e101d70bd88eacde7c
Sean Gilliam <sean.gilliam@gmail.com>
2023-11-01 00:59:35 +0000

    Added Serilog logging

---
+++ src/CommandersCall.Core/CommandersCall.Core/Logging/Logger.cs
@@ -0,0 +1,32 @@
+﻿namespace CommandersCall
+{
+	using Serilog;
+	using Serilog.Events;
+	using Serilog.Formatting.Json;
+
+	public static class Logger
+	{
+		private static ILogger _logger;
+
+		static Logger()
+		{
+			_logger = new LoggerConfiguration()
+				.WriteTo.Console(restrictedToMinimumLevel: LogEventLevel.Debug)
+				.WriteTo.Logger(config => config.Filter
+					.ByIncludingOnly(evt => evt.Level == LogEventLevel.Debug)
+					.WriteTo.File(
+						formatter: new JsonFormatter(),
+						path: "bugs.log"))
+				.WriteTo.File(
+					formatter: new JsonFormatter(),
+					path: "commanders.log",
+					restrictedToMinimumLevel: LogEventLevel.Information, 
+					rollingInterval: RollingInterval.Day)
+				.MinimumLevel.Debug()
+				.CreateLogger();
+		}
+
+		public static ILogger Entry => _logger;
+		
+	}
+}
--- src/CommandersCall.Service/CommandersCall.Service/Program.cs
+++ src/CommandersCall.Service/CommandersCall.Service/Program.cs
@@ -1,2 +1,8 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using CommandersCall;
+
 Console.WriteLine("Hello, World!");
+Logger.Entry.Information("Starting Commander's Call...");
+
+string bug = "oops";
+Logger.Entry.Debug("Some bug occurred {bug}", bug);
